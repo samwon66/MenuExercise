8820bd49e9d0b969404dcd0769fc0c66e217f5be
Sammy Wong <sammywong528@outlook.com>
2023-09-14 15:46:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MenuExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool userChoice = true;
+           
+            while (true)
+            {
+                Console.WriteLine("Welcome to main menu for testing differént tasks!");
+                Console.WriteLine("Please press a key to continue or press 0 to quit.");
+                int input = int.Parse(Console.ReadLine());
+                if (input == 0)
+                    userChoice = false;
+
+            }
+        }
+    }
+}
