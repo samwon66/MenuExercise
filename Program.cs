20a106d2ce28d2b4db8174deff123da69fbe174d
Sammy Wong <sammywong528@outlook.com>
2023-09-17 08:48:20 +0200

    Create class Ticket

--- Program.cs
+++ Program.cs
@@ -10,8 +10,10 @@
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to main menu for testing different tasks!");
-                Console.WriteLine("1. Get price of ticket");
-                Console.WriteLine("Please press a key to continue or press 0 to quit.");
+                Console.WriteLine("1. Price of ticket");
+                Console.WriteLine("2. The total price of tickets");
+                Console.WriteLine("0. Quit");
+                Console.Write(">");
                 int input = int.Parse(Console.ReadLine());
                 switch (input)
                 {
@@ -23,7 +25,7 @@
                     case 1:
                         if (input == 1)
                         {
-                            Console.WriteLine("Please enter your age: ");
+                            Console.Write("Please enter your age: ");
                             int userAge = int.Parse(Console.ReadLine());
                             if (userAge < 20)
                             {
@@ -43,7 +45,18 @@
                         }
                         break;
 
+                    case 2:
+                        Console.Write("How many tickets? ");
+                        int amountOfTickets = int.Parse(Console.ReadLine());
+                        for (int i = amountOfTickets; i > 0; i--)
+                        {
+                            
+                        }
+                        break;
+
                     default: 
+                        Console.WriteLine("Wrong input, please try again by pressing enter.");
+                        Console.ReadLine() ;
                         quitMenu = true; 
                         break;
 
